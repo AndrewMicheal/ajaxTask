fdb149efb2b24877bcee44910a7f92dd0a96d16d
AndrewMicheal <andrewmicheal2015@gmail.com>
2021-10-08 23:44:23 +0200

    ajax task

---
+++ js/ajax.js
@@ -0,0 +1,55 @@
+var buttonsContainer = document.getElementById("btns");
+
+function getAllUsersName() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/users");
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+            var users = JSON.parse(this.responseText);
+            showButtons(users);
+        }
+    }
+}
+
+getAllUsersName();
+
+function showButtons(usersName) {
+    var btns = ``;
+    for(var i = 0; i < usersName.length; i++) {
+        btns+= `<button class = "btn" id = "${usersName[i].id}" onclick = getUserPost(this)>${usersName[i].name}</button>`;
+    }
+    buttonsContainer.innerHTML = btns;
+}
+
+
+function getUserPost(btn) {
+    var userId = btn.getAttribute("id");
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET",`https://jsonplaceholder.typicode.com/posts/${userId}`);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+            var userPost = JSON.parse(this.responseText);
+            displayUserPost(userPost);
+            btnActive(btn);
+        }
+    }
+}
+
+function displayUserPost(userPost) {
+    var userTitle = document.getElementById("userTitle");
+    var liContainer = `<li>${userPost.body}</li>`;
+    userTitle.innerHTML = liContainer; 
+}
+
+function btnActive(button) {
+    var btns = document.querySelectorAll(".btn");
+    for(var i = 0; i < btns.length; i++) {
+        if (button.getAttribute("id") == btns[i].getAttribute("id")) {
+            btns[i].classList.add("active");
+        } else {
+            btns[i].classList.remove("active");
+        }
+    }
+}
